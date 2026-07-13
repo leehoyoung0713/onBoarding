1df87108942e900e58bfbc4c5649139e9d4d35af
leehoyoung <whei1234@naver.com>
2026-07-13 17:20:41 +0900

    Create text_textarea.vue

---
+++ week2/form_example/text_textarea.vue
@@ -0,0 +1,10 @@
+vue<script setup>
+import { ref } from 'vue'
+const name = ref('')
+const message = ref('')
+</script>
+
+<template>
+  <input type="text" v-model="name" />
+  <textarea v-model="message"></textarea>
+</template>
