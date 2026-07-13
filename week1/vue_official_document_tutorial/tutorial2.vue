7f3dbb071eb37af1e32008f5ab483809ad71c061
leehoyoung <whei1234@naver.com>
2026-07-13 17:07:30 +0900

    Create tutorial2.vue

---
+++ week1/vue_official_document_tutorial/tutorial2.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({ count: 0 })
+const message = ref('안녕 Vue!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p>숫자 세기: {{ counter.count }}</p>
+</template>
