6c4ffa671627d6ad5cb9573ecadf584f3b785fab
leehoyoung <whei1234@naver.com>
2026-07-13 17:07:51 +0900

    Create tutorial3.vue

---
+++ week1/vue_official_document_tutorial/tutorial3.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">나를 빨갛게 만들어 보세요</h1>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
