cfa72a1490e27621cc4a885b6b4f71dc5e22daca
hoyoung <whei1234@naver.com>
2026-07-14 10:54:30 +0900

    week3_day2 content

---
+++ week3/inflearn_typescript/day2/class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+// JS 문자열 선언
+// var str='hello';
+
+// TS 문자열 선언
+let str: string='hello';
+
+// TS 숫자
+let num: number=10;
+
+// TS배열
+let arr: Array<number>=[1,2,3];
+let heroes: Array<string>=['Capt','Thor','Hulk',10];
+let items: number[]=[1,2,3];
+
+// TS 튜플
+// 각 요소의 타입이 지정되어 있는 배열 형식
+let address: [string,number]=['gangnam',100];
+
+// TS 객체
+let obj: object={};
+// let person: object={
+//     name:'capt',
+//     age:100
+// };
+// TS 객체 구체화
+let person: {name: string, age:number}={
+    name: 'thor',
+    age: 1000
+}
+
+// TS 진위값
+let show: boolean=true;
